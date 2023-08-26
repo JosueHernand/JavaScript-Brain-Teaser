880d0cd5471e920e9f4766b8ff09fe22876646ad
Josue Hernandez <joseh1337@gmail.com>
2023-08-25 20:05:09 -0700

    made a countdown timer and a picture after time expires

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,4 +2,28 @@ const timeEl = document.querySelector(".time");
 
 const bombEl = document.getElementById("bomb");
 
-const secondsLeft = 80;
+let secondsLeft = 80;
+
+function setTime() {
+ 
+    const timerInterval = setInterval(function() {
+      secondsLeft--;
+      timeEl.textContent = "Time: " + secondsLeft;
+  
+      if(secondsLeft === 0) {
+        clearInterval(timerInterval);
+        sendMessage();
+      }
+  
+    }, 1000);
+  }
+  
+  function sendMessage() {
+     timeEl.textContent = " ";
+     const imgEl = document.createElement("img");
+     imgEl.setAttribute("src", "assets/images/explosion.jpg");
+     bombEl.appendChild(imgEl);
+  
+    }
+  
+  setTime();
