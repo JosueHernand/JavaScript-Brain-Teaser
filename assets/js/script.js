f8461623ab92a97fa54d7fa856856364485c386f
Josue Hernandez <joseh1337@gmail.com>
2023-08-27 18:25:46 -0700

    restructured the html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,39 +1,57 @@
 const timeEl = document.querySelector(".time");
 const startQuizButton = document.getElementById("start");
-const bombEl = document.getElementById("bomb");
 const quizContainer = document.getElementById("quiz");
+const bombEl = quizContainer.querySelector("#bomb");
+const answerButtons = document.querySelectorAll(".answer");
 
+let currentQuestionIndex = 0;
 let secondsLeft = 999;
 let interval;
 
-startQuizButton.addEventListener("click", startQuiz);
-
-function startQuiz() {
+startQuizButton.addEventListener("click", function () {
     startQuizButton.style.display = 'none';
-    quizContainer.style.display = 'block';
-    interval = setInterval(updateTimer, 1000)
-
-    const container = document .querySelector(".container");
+    const container = document.querySelector(".container");
     container.style.display = "none";
+    interval = setInterval(updateTimer, 1000);
+    showQuestion(questionIds[currentQuestionIndex]);
+});
+
+function showQuestion(questionId) {
+    const allQuestions = document.querySelectorAll("#quiz > div");
+    for (let i = 0; i < allQuestions.length; i++) {
+        allQuestions[i].style.display = "none";
+    }
+    document.getElementById(questionId).style.display = "block";
 }
+
+answerButtons.forEach(function(button) {
+    button.addEventListener("click", function() {
+        if (currentQuestionIndex < questionIds.length - 1) {
+            currentQuestionIndex++;
+            showQuestion(questionIds[currentQuestionIndex]);
+        }
+    });
+});
 
 function updateTimer() {
     secondsLeft--;
-    timeEl.textContent = "Time: " + secondsLeft
+    timeEl.textContent = "Time: " + secondsLeft;
 
     if (secondsLeft === 0) {
         clearInterval(interval);
         sendMessage();
+        
+        if (currentQuestionIndex < questionIds.length - 1) {
+            currentQuestionIndex++;
+            showQuestion(questionIds[currentQuestionIndex]);
+        }
     }
-  }
+}
   
-  function sendMessage() {
-     timeEl.textContent = " ";
-     const imgEl = document.createElement("img");
-     imgEl.setAttribute("src", "assets/images/explosion.jpg");
-     imgEl.width = 500;
-     bombEl.appendChild(imgEl);
-  
-    }
-  
-  setTime();
+function sendMessage() {
+    timeEl.textContent = " ";
+    const imgEl = document.createElement("img");
+    imgEl.setAttribute("src", "assets/images/explosion.jpg");
+    imgEl.width = 500;
+    bombEl.appendChild(imgEl);
+}
